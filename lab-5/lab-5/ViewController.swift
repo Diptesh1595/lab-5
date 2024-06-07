78f24ee2b4742a27cd7a2b996f0ab74b4ea8302d
Diptesh1595 <dpatel1595@conestogac.on.ca>
2024-06-07 12:01:59 -0700

    initial commit

---
+++ lab-5/lab-5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab-5
+//
+//  Created by user252703 on 6/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
